6b74cb1a11709df89cb2f98d8bd72852fcdc9dbb
Banele <banelempofu7979@gmail.com>
2023-09-11 15:10:04 +0200

    timer

--- src/app/COMPONENTS/countdown/countdown.component.ts
+++ src/app/COMPONENTS/countdown/countdown.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 
-import { CountdownConfig } from 'ngx-countdown';
+import { CountdownConfig, CountdownEvent } from 'ngx-countdown';
 
 @Component({
   selector: 'app-countdown',
@@ -10,9 +10,25 @@ import { CountdownConfig } from 'ngx-countdown';
   changeDetection: ChangeDetectionStrategy.OnPush, //????
 })
 export class CountdownComponent {
-  config: CountdownConfig = { stopTime: new Date().getTime() + 1000 * 60 };
+  // config: CountdownConfig = { stopTime: new Date().getTime() + 1000 * 600 };
 
-  resetStop() {
-    this.config = { stopTime: new Date().getTime() + 1000 * 60 };
+  // resetStop() {
+  //   this.config = { stopTime: new Date().getTime() + 1000 * 600 };
+  // }
+
+  // -----------------------------------------------------------
+  config: CountdownConfig = {
+    leftTime: 60,
+    format: 'HH:mm:ss',
+    prettyText: (text) => {
+      return text
+        .split(':')
+        .map((v) => `<span class="item">${v}</span>`)
+        .join('');
+    },
+  };
+
+  handleEvent(e: CountdownEvent) {
+    console.log('Actions', e);
   }
 }
